36e6cc4f2dcaa69685dfc8711615eca3a39fd8ad
2020212205323王庆典 <104480668+wqdcjnb@users.noreply.github.com>
2023-06-29 08:27:44 +0800

    Update Index.vue

--- project/file-frontend/src/components/Index.vue
+++ project/file-frontend/src/components/Index.vue
@@ -7,6 +7,15 @@
         <button type="submit">登录</button>
       </form>
     </div>
+      <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+        <el-button class="button" text>Operation button</el-button>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+  </el-card>
   </template>
   
   <script>
@@ -60,5 +69,22 @@
     border-radius: 4px;
     cursor: pointer;
   }
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 480px;
+}
   </style>
   
